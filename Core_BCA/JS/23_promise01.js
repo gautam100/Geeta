28356d65e76189304b66057554dab6cbad96f04d
Gautam Mukherjee <mukherjeegautam100@gmail.com>
2026-03-27 13:14:23 +0530

    promise

---
+++ Core_BCA/JS/23_promise01.js
@@ -0,0 +1,27 @@
+function MyPromise() {
+  return new Promise(function (resolve, reject) {
+    let result = Math.random() > 0.5;
+    if (result === true) {
+      resolve("Promise is successful");
+    } else {
+      reject("Promise is unsuccessful");
+    }
+  });
+}
+
+MyPromise()
+  .then(function (msg) {
+    console.log(msg);
+  })
+  .catch(function (msg) {
+    console.log(msg);
+  })
+  .finally(function () {
+    console.log("The End");
+  });
+/*
+Promise states
+1) pending
+2) fullfilled
+3) rejected
+*/
